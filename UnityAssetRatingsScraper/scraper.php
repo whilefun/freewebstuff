3fab0b5bdf83891d02ce197bb1c4acf844dd2741
Richard Walsh <git@whilefun.com>
2021-09-25 17:25:57 -0400

    -patch fixes for September 2021 Unity Asset Store page style refresh that broke previous version.

--- UnityAssetRatingsScraper/scraper.php
+++ UnityAssetRatingsScraper/scraper.php
@@ -13,19 +13,21 @@
 	
 	// List your product(s) here
 	$assetStoreUrl_productName = 'https://assetstore.unity.com/packages/essentials/asset-packs/standard-assets-32351';
+	$assetStoreUrl_productName_reviews = 'https://assetstore.unity.com/packages/essentials/asset-packs/standard-assets-32351/reviews';
 	
 	?>
 	
 	<h3>Product Name Here (<a href="<?php print_r($assetStoreUrl_productName); ?>" target="assetStore">Asset Store Page</a>)</h3>
 	<?php
 	print(FetchRatingString($assetStoreUrl_productName));
+	print(FetchReviewString($assetStoreUrl_productName_reviews));
 	?>
 	<hr>
 
 	<?php 
 	function FetchRatingString($pageUrl)
 	{
-				
+		
 		$page = file_get_contents($pageUrl);
 		$ratingString = "<ul>";
 
@@ -36,7 +38,6 @@
 		$outlineStarToken = "ifont-star-outline";
 		$fullStarChar = "★";
 		$outlineStarChar = "☆";
-		
 		
 		// Star Rating //
 		//
@@ -64,23 +65,55 @@
 			}
 
 		}
-		
-		$ratingString .= '<li>' . $starString . '  (' . $totalStarCount . ' out of 5)</li>';
 
+		$ratingString .= '<li>' . $starString . '  (' . $totalStarCount . ' out of 5)';
 		
-		// User Review Count //
+		// User Rating Count //
 		//
 		// As of December 2019 store page style refresh, reviews are no longer formatted "N user reviews". It is now just "N Reviews"
-		
-		$userReviewCount = '/([\d]* Reviews)/';
-		preg_match($userReviewCount, $page, $reviews);
-		$ratingString .= '<li>' . $reviews[0] . '</li>';
-		$ratingString .= "</ul>";
+		//
+		// As of September 2021 store page style refresh, reviews are no longer formatter "N Reviews". It is just stars and (N) where N is number of user ratings
+
+		// Look for a number in parentheses
+		$userReviewCountPattern = '/\(([\d]*)\)/';
+		// And extend breadth of substring search to account for stars plus some "(N)"
+		$reviewCountTokenZoneExtendedLength = $starTokenZoneLength + 20;
+		$reviewCountSubstring = substr($page, $roughStarStartPosition, $reviewCountTokenZoneExtendedLength);
+		preg_match($userReviewCountPattern, $reviewCountSubstring, $ratingCountResults);
+		$scrubbedRatingsCount = str_replace(array('(', ')'), '', $ratingCountResults[0]);
+		$ratingString .= ' (' . $scrubbedRatingsCount. ' User Ratings)</li>';
+
 		return $ratingString;
-		
 		
 	}
 	?>
 
+	<?php 
+	function FetchReviewString($pageUrl)
+	{
+				
+		$page = file_get_contents($pageUrl);
+		$reviewString = "";
+
+		// For review count, we use the star rating followed by string such as "N User Reviews", about 395 chars
+		$postStarCharactersToReviewCount = 600;
+		$fullStarToken = "ifont-star";
+		
+		// User Reviews //
+		// Approximate start position of the star rating area (for efficiency)
+		$roughStarStartPosition = strpos($page, $fullStarToken);
+		$reviewCountSubtstring = substr($page, $roughStarStartPosition, $postStarCharactersToReviewCount);
+		
+		$userReviewCountPattern = '/([\d]*) User Reviews/';
+		preg_match($userReviewCountPattern, $reviewCountSubtstring, $userReviewCountResults);
+		$reviewString .= '<li>' . $userReviewCountResults[0] . '</li>';
+		$reviewString .= "</ul>";
+		
+		return $reviewString;
+		
+	}
+	?>
+
+	
 	</body>
 </html>
